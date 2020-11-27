f9a7263453f149f7e38da1704f81751615e79ffa
Yassine Bridi <ybridi@gmail.com>
2020-11-27 14:18:44 +0100

    Make h and l to be move left and right accordignly

--- dmenu.c
+++ dmenu.c
@@ -392,8 +392,8 @@ keypress(XKeyEvent *ev)
 		case XK_g: ksym = XK_Home;  break;
 		case XK_G: ksym = XK_End;   break;
 		case XK_k: ksym = XK_Up;    break;
-		case XK_l: ksym = XK_Next;  break;
-		case XK_h: ksym = XK_Prior; break;
+		case XK_l: ksym = XK_Right;  break;
+		case XK_h: ksym = XK_Left; break;
 		case XK_j: ksym = XK_Down;  break;
 		case XK_v:
 			XConvertSelection(dpy, (ev->state) ? clip : XA_PRIMARY,
