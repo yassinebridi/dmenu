e05a5447582b005ee82381d1edd44a3498c48b92
Yassine Bridi <ybridi@gmail.com>
2021-01-10 17:18:34 +0100

    Gruvbox thee

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#2e3440" },
+	[SchemeNorm] = { "#bbbbbb", "#ceac01" },
 	[SchemeSel] = { "#eeeeee", "#88c0d0" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
